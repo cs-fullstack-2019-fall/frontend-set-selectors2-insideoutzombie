da5d29ccd075d6a35403c54f762048b24c7012eb
autumn-ragland <raglanda998@gmail.com>
2019-09-10 14:49:22 -0500

    Graded 09-10-2019 14:48:37

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 // 1) Print the first p tag to the console using the ID, class, and tag selectors.
 let printP = document.getElementById('first');
+// !! : printP.innerText
 console.log(printP);
 
 let printD = document.getElementsByClassName("special");
+// !! : printD[0].innerText
 console.log(printD);
 
 console.log(document.getElementsByTagName("special"));
+// !! : getElememts returns an array
 
 
 // 2) Print the special class using both the query selector and query all selector.
 
 var el = document.querySelector(".special");
 console.log(el);
+// !! : el.innerText
 let el2 = document.querySelectorAll(".special")
 console.log(el2);
+// !! : el2[0].innerText
 
 
 
